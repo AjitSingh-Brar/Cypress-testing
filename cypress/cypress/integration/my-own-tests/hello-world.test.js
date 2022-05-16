99b89614c7d62cc6d87d7d3efa823dababf968bc
Ajit Brar <abrar@trigon.ca>
2022-05-16 09:48:03 -0400

    Tested cy.viewport

--- cypress/cypress/integration/my-own-tests/hello-world.test.js
+++ cypress/cypress/integration/my-own-tests/hello-world.test.js
@@ -2,11 +2,25 @@
 
 describe('Basic test', () => {
     
+
     it('We have correct page title', () => {
+        cy.viewport('iphone-5');
         cy.visit('https://codedamn.com');
        
-        // mocha 
+        // mocha , way 1
         cy.contains('Learn Programming'). should('exist');
+
+        //cy.get way 2
+         cy.get('div[id=root]').should("exist");
+         cy.get('div#noroot').should("not.exist");
+
+         //way 3 (this should be considered always for the best practise 
+         //during development time to production time)
+         cy.get('[data-testid=submit-btn]');
+
+         cy.contains("Learn Programming").should('have.text', 'Learn Programming')
     })
+
+    
 
 })
